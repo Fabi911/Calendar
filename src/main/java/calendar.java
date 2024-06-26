3b34a1146795952beabbf78fbea32c54dcc3471a
Fabian Döz <fabian@doez.info>
2024-06-26 17:11:48 +0200

    finish calendar

--- src/main/java/calendar.java
+++ src/main/java/calendar.java
@@ -1,19 +1,32 @@
 public class calendar {
+
     public static void main(String[] args) {
         String[] months = {"January", "February", "March", "April", "May", "June",
                 "July", "August", "September", "October", "November", "December"};
         int[] daysInMonth = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        int[] offsetDays = {0, 3, 4, 0, 2, 5, 0, 3, 6, 1, 4, 6}; // Example offsets
         for (int i = 0; i < months.length; i++) {
-            printCalendarForMonth(months[i], daysInMonth[i]);
+            printCalendarForMonth(months[i], daysInMonth[i], offsetDays[i]);
         }
     }
 
-    public static void printCalendarForMonth(String monthName, int daysInMonth) {
+    public static void printCalendarForMonth(String monthName, int daysInMonth, int offsetDays) {
         System.out.println(monthName);
         System.out.println("Mo Tu We Th Fr Sa Su");
         int day = 1;
-        for (int i = 0; i < 6; i++) {
-            for (int j = 0; j < 7; j++) {
+
+        for (int i = 0; i < offsetDays; i++) {
+            System.out.print("   ");
+        }
+        
+        for (int i = offsetDays; i < 7; i++) {
+            System.out.printf("%2d ", day);
+            day++;
+        }
+        System.out.println();
+
+        while (day <= daysInMonth) {
+            for (int i = 0; i < 7; i++) {
                 if (day <= daysInMonth) {
                     System.out.printf("%2d ", day);
                     day++;
@@ -22,9 +35,7 @@ public class calendar {
                 }
             }
             System.out.println();
-            if (day > daysInMonth) {
-                break;
-            }
         }
     }
 }
+
