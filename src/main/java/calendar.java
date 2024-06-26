0d1fd69405072b603da3331d7dfc54d419440377
Fabian Döz <fabian@doez.info>
2024-06-26 16:48:24 +0200

    exenteds method for continues counting

--- src/main/java/calendar.java
+++ src/main/java/calendar.java
@@ -1,20 +1,30 @@
 public class calendar {
-    public static void printCalendarForMonth(String monthName){
-        System.out.println(monthName);
-        System.out.println(" 1  2  3  4  5  6  7");
-    }
     public static void main(String[] args) {
-printCalendarForMonth("January");
-    printCalendarForMonth("February");
-    printCalendarForMonth("March");
-    printCalendarForMonth("April");
-    printCalendarForMonth("May");
-    printCalendarForMonth("June");
-    printCalendarForMonth("July");
-    printCalendarForMonth("August");
-    printCalendarForMonth("September");
-    printCalendarForMonth("October");
-    printCalendarForMonth("November");
-    printCalendarForMonth("December");
+        String[] months = {"January", "February", "March", "April", "May", "June",
+                "July", "August", "September", "October", "November", "December"};
+        int[] daysInMonth = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        for (int i = 0; i < months.length; i++) {
+            printCalendarForMonth(months[i], daysInMonth[i]);
+        }
+    }
+
+    public static void printCalendarForMonth(String monthName, int daysInMonth) {
+        System.out.println(monthName);
+        System.out.println("Mo Tu We Th Fr Sa Su");
+        int day = 1;
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 7; j++) {
+                if (day <= daysInMonth) {
+                    System.out.printf("%2d ", day);
+                    day++;
+                } else {
+                    System.out.print("   ");
+                }
+            }
+            System.out.println();
+            if (day > daysInMonth) {
+                break;
+            }
+        }
     }
 }
