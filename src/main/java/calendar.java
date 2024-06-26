9f28a36bdeb8ba3888a130ad0c10de4647585096
vokarl <karl.vogl@gmx.de>
2024-06-26 16:03:12 +0200

    add method for months output

--- src/main/java/calendar.java
+++ src/main/java/calendar.java
@@ -1,5 +1,19 @@
 public class calendar {
+    public static void printCalendarForMonth(String monthName){
+System.out.println(monthName);
+    }
     public static void main(String[] args) {
-
+printCalendarForMonth("January");
+    printCalendarForMonth("February");
+    printCalendarForMonth("March");
+    printCalendarForMonth("April");
+    printCalendarForMonth("May");
+    printCalendarForMonth("June");
+    printCalendarForMonth("July");
+    printCalendarForMonth("August");
+    printCalendarForMonth("September");
+    printCalendarForMonth("October");
+    printCalendarForMonth("November");
+    printCalendarForMonth("December");
     }
 }
